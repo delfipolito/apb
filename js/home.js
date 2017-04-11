da9b8c2d76817ae520486844d4c4f1b9668001c3
Delfina <delfinapolito@gmail.com>
2017-04-11 10:38:04 -0300

    projects section

--- js/home.js
+++ js/home.js
@@ -91,4 +91,92 @@ $( document ).ready(function(){
 		}, 5000);
 
 
+		$("#area1").mouseenter( function() {
+      $('.caesarea-triangle').removeClass('hidden');
+	  });
+	  $("#close-caesarea").click( function() {
+	  	$('.caesarea-triangle').addClass('hidden');
+	  });
+
+	  $("#area2").mouseenter( function() {
+	  	$('.furniture-triangle').removeClass('hidden');
+	  });
+	  $("#close-furniture").click( function() {
+	  	$('.furniture-triangle').addClass('hidden');
+	  });
+
+	  $("#area3").mouseenter( function() {
+	  	$('.mallorca-triangle').removeClass('hidden');
+	  });
+	  $("#close-mallorca").click( function() {
+	  	$('.mallorca-triangle').addClass('hidden');
+	  });
+
+	  $("#area4").mouseenter( function() {
+	  	$('.london-triangle').removeClass('hidden');
+	  });
+	  $("#close-london").click( function() {
+	  	$('.london-triangle').addClass('hidden');
+	  });
+
+	  $("#area5").mouseenter( function() {
+	  	$('.bsas-triangle').removeClass('hidden');
+	  });
+	  $("#close-bsas").click( function() {
+	  	$('.bsas-triangle').addClass('hidden');
+	  });
+
+	  $("#area6").mouseenter( function() {
+	  	$('.sfernando-triangle').removeClass('hidden');
+	  });
+	  $("#close-sfernando").click( function() {
+	  	$('.sfernando-triangle').addClass('hidden');
+	  });
+
+	  $("#area7").mouseenter( function() {
+	  	$('.bahamas-tirangle').removeClass('hidden');
+	  });
+	  $("#close-bahamas").click( function() {
+	  	$('.bahamas-tirangle').addClass('hidden');
+	  });
+
+	  $("#area8").mouseenter( function() {
+	  	$('.aspen-triangle').removeClass('hidden');
+	  });
+	  $("#close-aspen").click( function() {
+	  	$('.aspen-triangle').addClass('hidden');
+	  });
+
+	  $("#area9").mouseenter( function() {
+	  	alert("area9");
+	  });
+	  $("#area9").mouseleave( function() {
+	  });
+
+	  // transitions
+	  $("#aspen-transition-1").click( function() {
+	  	$('.aspen1-triangle').removeClass('hidden');
+	  	$('.aspen1-triangle').animate({height: "285px"});
+	  });
+	  $("#aspen-transition-2").click( function() {
+	  	$('.aspen2-triangle').removeClass('hidden');
+	  	$('.aspen2-triangle').animate({height: "285px"});
+	  });
+	  $("#aspen-transition-3").click( function() {
+	  	$('.aspen3-triangle').removeClass('hidden');
+	  	$('.aspen3-triangle').animate({height: "285px"});
+	  });
+
+	  $("#london-transition-1").click( function() {
+	  	$('.london1-triangle').removeClass('hidden');
+	  	$('.london1-triangle').animate({height: "285px"});
+	  });
+	  $("#london-transition-2").click( function() {
+	  	$('.london2-triangle').removeClass('hidden');
+	  	$('.london2-triangle').animate({height: "285px"});
+	  });
+	  $("#london-transition-3").click( function() {
+	  	$('.london3-triangle').removeClass('hidden');
+	  	$('.london3-triangle').animate({height: "285px"});
+	  });
 })
