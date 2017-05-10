68eb725d068c9b55dda65a4ea8abfa9514bdaac8
Delfina <delfinapolito@gmail.com>
2017-05-10 15:05:56 -0300

    cover

--- js/home.js
+++ js/home.js
@@ -8,185 +8,178 @@ $( document ).ready(function(){
 	  if ($(window).scrollTop() >= (caesarea - 120) && $(window).scrollTop() <= (caesarea + 400)){	
 	  	$('#animate1').addClass('item-hovered');
 	  } 
-	  if ($(window).scrollTop() ==  $('.caesarea-item').outerHeight()/2){
+	  if ($(window).scrollTop() >=  caesarea + 600){
 	  	$('#animate1').removeClass('item-hovered');
 	  }
 	  var belair = $('.belair-item').offset().top;
-	  if ($(window).scrollTop() == (belair - 120)){
+	  if ($(window).scrollTop() >= (belair - 120) && $(window).scrollTop() <= (belair + 400)){
 	  	$('#animate3').addClass('item-hovered');
 	  } 
-	  if ($(window).scrollTop() ==  $('.belair-item').outerHeight()/2){
+	  if ($(window).scrollTop() >=  belair + 600){
 	  	$('#animate3').removeClass('item-hovered');
 	  }
 
 	  var aspen1 = $('.aspen1-img-2').offset().top;
-	  if ($(window).scrollTop() == (aspen1 - 120)){
+	  if ($(window).scrollTop() >= (aspen1 - 120) && $(window).scrollTop() <= (aspen1 + 400)){
 	  	$('#animate2').addClass('item-hovered');
 	  } 
-	  if ($(window).scrollTop() ==  $('.aspen1-img-2').outerHeight()/2){
+	  if ($(window).scrollTop() >=  aspen1 + 600){
 	  	$('#animate2').removeClass('item-hovered');
 	  }
 	  
 	  var aspen2 = $('.aspen2-img-1').offset().top;
-	  if ($(window).scrollTop() == (aspen2 - 120)){
+	  if ($(window).scrollTop() >= (aspen2 - 120) && $(window).scrollTop() <= (aspen2 + 400)){
 	  	$('#animate4').addClass('item-hovered');
 	  } 
-	  if ($(window).scrollTop() ==  $('.aspen2-img-1').outerHeight()/2){
+	  if ($(window).scrollTop() >=  aspen2 + 600){
 	  	$('#animate4').removeClass('item-hovered');
 	  }
 	});
 
 
 
-		$("#area1").mouseenter( function() {
-      $('.caesarea-triangle').removeClass('hidden');
-	  });
-	  $("#close-caesarea").click( function() {
-	  	$('.caesarea-triangle').addClass('hidden');
-	  	hideContent();
-	  });
+	$("#area1").mouseenter( function() {
+    $('.caesarea-triangle').removeClass('hidden');
+  });
+  $("#close-caesarea").click( function() {
+  	$('.caesarea-triangle').addClass('hidden');
+  	hideContent();
+  });
 
-	  $("#area2").mouseenter( function() {
-	  	$('.furniture-triangle').removeClass('hidden');
-	  });
-	  $("#close-furniture").click( function() {
-	  	$('.furniture-triangle').addClass('hidden');
-	  	hideContent();
-	  });
+  $("#area2").mouseenter( function() {
+  	$('.furniture-triangle').removeClass('hidden');
+  });
+  $("#close-furniture").click( function() {
+  	$('.furniture-triangle').addClass('hidden');
+  	hideContent();
+  });
 
-	  $("#area3").mouseenter( function() {
-	  	$('.mallorca-triangle').removeClass('hidden');
-	  });
-	  $("#close-mallorca").click( function() {
-	  	$('.mallorca-triangle').addClass('hidden');
-	  	hideContent();
-	  });
+  $("#area3").mouseenter( function() {
+  	$('.mallorca-triangle').removeClass('hidden');
+  });
+  $("#close-mallorca").click( function() {
+  	$('.mallorca-triangle').addClass('hidden');
+  	hideContent();
+  });
 
-	  $("#area4").mouseenter( function() {
-	  	$('.london-triangle').removeClass('hidden');
-	  });
-	  $("#close-london").click( function() {
-	  	$('.london-triangle').addClass('hidden');
-	  	hideContent();
-	  });
+  $("#area4").mouseenter( function() {
+  	$('.london-triangle').removeClass('hidden');
+  });
+  $("#close-london").click( function() {
+  	$('.london-triangle').addClass('hidden');
+  	hideContent();
+  });
 
-	  $("#area5").mouseenter( function() {
-	  	$('.bsas-triangle').removeClass('hidden');
-	  });
-	  $("#close-bsas").click( function() {
-	  	$('.bsas-triangle').addClass('hidden');
-	  	hideContent();
-	  });
+  $("#area5").mouseenter( function() {
+  	$('.bsas-triangle').removeClass('hidden');
+  });
+  $("#close-bsas").click( function() {
+  	$('.bsas-triangle').addClass('hidden');
+  	hideContent();
+  });
 
-	  $("#area6").mouseenter( function() {
-	  	$('.sfernando-triangle').removeClass('hidden');
-	  });
-	  $("#close-sfernando").click( function() {
-	  	$('.sfernando-triangle').addClass('hidden');
-	  	hideContent();
-	  });
+  $("#area6").mouseenter( function() {
+  	$('.sfernando-triangle').removeClass('hidden');
+  });
+  $("#close-sfernando").click( function() {
+  	$('.sfernando-triangle').addClass('hidden');
+  	hideContent();
+  });
 
-	  $("#area7").mouseenter( function() {
-	  	$('.bahamas-tirangle').removeClass('hidden');
-	  });
-	  $("#close-bahamas").click( function() {
-	  	$('.bahamas-tirangle').addClass('hidden');
-	  	hideContent();
-	  });
+  $("#area7").mouseenter( function() {
+  	$('.bahamas-tirangle').removeClass('hidden');
+  });
+  $("#close-bahamas").click( function() {
+  	$('.bahamas-tirangle').addClass('hidden');
+  	hideContent();
+  });
 
-	  $("#area8").mouseenter( function() {
-	  	$('.aspen-triangle').removeClass('hidden');
-	  });
-	  $("#close-aspen").click( function() {
-	  	$('.aspen-triangle').addClass('hidden');
-	  	hideContent();
-	  });
+  $("#area8").mouseenter( function() {
+  	$('.aspen-triangle').removeClass('hidden');
+  });
+  $("#close-aspen").click( function() {
+  	$('.aspen-triangle').addClass('hidden');
+  	hideContent();
+  });
 
-	  $("#area9").mouseenter( function() {
-	  	$('.la-triangle').removeClass('hidden');
-	  });
-	  $("#close-la").click( function() {
-	  	$('.la-triangle').addClass('hidden');
-	  	hideContent();
-	  });
+  $("#area9").mouseenter( function() {
+  	$('.la-triangle').removeClass('hidden');
+  });
+  $("#close-la").click( function() {
+  	$('.la-triangle').addClass('hidden');
+  	hideContent();
+  });
 
 
-	  $("#marea4").click( function() {
-	  	alert('london');
-	  	console.log("hola");
-	  });
-	  $("#area4").click( function() {
-	  	alert('london');
-	  	console.log("hola");
-	  });
+	  
 
-	  // transitions
-	  $("#aspen-transition-1").click( function() {
-	  	$('.aspen1-triangle').removeClass('hidden');
-	  	$('.aspen1-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#aspen-transition-2").click( function() {
-	  	$('.aspen2-triangle').removeClass('hidden');
-	  	$('.aspen2-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#aspen-transition-3").click( function() {
-	  	$('.aspen3-triangle').removeClass('hidden');
-	  	$('.aspen3-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
+  // transitions
+  $("#aspen-transition-1").click( function() {
+  	$('.aspen1-triangle').removeClass('hidden');
+  	$('.aspen1-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#aspen-transition-2").click( function() {
+  	$('.aspen2-triangle').removeClass('hidden');
+  	$('.aspen2-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#aspen-transition-3").click( function() {
+  	$('.aspen3-triangle').removeClass('hidden');
+  	$('.aspen3-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
 
-	  $("#london-transition-1").click( function() {
-	  	$('.london1-triangle').removeClass('hidden');
-	  	$('.london1-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#london-transition-2").click( function() {
-	  	$('.london2-triangle').removeClass('hidden');
-	  	$('.london2-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#london-transition-3").click( function() {
-	  	$('.london3-triangle').removeClass('hidden');
-	  	$('.london3-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#bsas-transition-1").click( function() {
-	  	$('.bsas1-triangle').removeClass('hidden');
-	  	$('.bsas1-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#bsas-transition-2").click( function() {
-	  	$('.bsas2-triangle').removeClass('hidden');
-	  	$('.bsas2-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#la-transition-1").click( function() {
-	  	$('.la1-triangle').removeClass('hidden');
-	  	$('.la1-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#la-transition-2").click( function() {
-	  	$('.la2-triangle').removeClass('hidden');
-	  	$('.la2-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#la-transition-3").click( function() {
-	  	$('.la3-triangle').removeClass('hidden');
-	  	$('.la3-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#la-transition-4").click( function() {
-	  	$('.la4-triangle').removeClass('hidden');
-	  	$('.la4-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
-	  $("#la-transition-5").click( function() {
-	  	$('.la5-triangle').removeClass('hidden');
-	  	$('.la5-triangle').animate({height: "285px"}, "slow", "swing");
-	  	showContent();
-	  });
+  $("#london-transition-1").click( function() {
+  	$('.london1-triangle').removeClass('hidden');
+  	$('.london1-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#london-transition-2").click( function() {
+  	$('.london2-triangle').removeClass('hidden');
+  	$('.london2-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#london-transition-3").click( function() {
+  	$('.london3-triangle').removeClass('hidden');
+  	$('.london3-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#bsas-transition-1").click( function() {
+  	$('.bsas1-triangle').removeClass('hidden');
+  	$('.bsas1-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#bsas-transition-2").click( function() {
+  	$('.bsas2-triangle').removeClass('hidden');
+  	$('.bsas2-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#la-transition-1").click( function() {
+  	$('.la1-triangle').removeClass('hidden');
+  	$('.la1-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#la-transition-2").click( function() {
+  	$('.la2-triangle').removeClass('hidden');
+  	$('.la2-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#la-transition-3").click( function() {
+  	$('.la3-triangle').removeClass('hidden');
+  	$('.la3-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#la-transition-4").click( function() {
+  	$('.la4-triangle').removeClass('hidden');
+  	$('.la4-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
+  $("#la-transition-5").click( function() {
+  	$('.la5-triangle').removeClass('hidden');
+  	$('.la5-triangle').animate({height: "285px"}, "slow", "swing");
+  	showContent();
+  });
 
 	function showContent() {
 		setTimeout(function () {
